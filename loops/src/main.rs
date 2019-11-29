c779018c494e14f93816562a9bbadc411d47bb18
Blake Sager <blake.sager@activeprospect.com>
2019-11-29 09:14:35 -0600

    finish loops section

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -19,4 +19,10 @@ fn main() {
         number -= 1;
     }
     println!("Liftoff!");
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("The value is {}", element)
+    }
 }
